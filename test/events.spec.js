b926fd9111c4050bea8b01acf215706f2a7604d4
qysp <qysp.yo@gmail.com>
2019-05-04 23:27:59 +0200

    add events test

---
+++ test/events.spec.js
@@ -0,0 +1,29 @@
+const expect = require('expect.js');
+const EzeventEmitter = require('../lib/ezevents');
+
+describe('EventEmitter', function() {
+  const emitter = new EzeventEmitter();
+  let listener;
+
+  it('should add a listener and emit it', function(done) {
+    listener = emitter.on('test', done);
+    emitter.emit('test');
+  });
+
+  it('should remove the added listener', function() {
+    emitter.removeListener('test', listener);
+    expect(emitter.events['test']).to.be.empty();
+  });
+
+  it('should add an async listener and emit it (wait 100ms)', async function() {
+    let val = false;
+
+    emitter.addListener('async', async () => {
+      await new Promise(resolve => setTimeout(resolve, 100))
+        .then(() => val = true);
+    });
+    await emitter.emit('async');
+
+    expect(val).to.be(true);
+  });
+});

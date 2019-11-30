97d75f5af72d9bd5a269bb3c9b80da43dd45dcc9
qysp <dragoncia@web.de>
2019-11-30 13:52:07 +0100

    enhance schema validation tests

--- test/schema.spec.js
+++ test/schema.spec.js
@@ -3,19 +3,23 @@ const Schema = require('../lib/schema');
 
 describe('Schema', function() {
   const createSchema = () => new Schema({
-    price: {
-      min: 'number',
-      max: 'number',
-      avg: {
-        __value: 'number',
-        __optional: true,
-      }
+    any: 'any',
+    string: 'string',
+    number: 'number',
+    boolean: 'boolean',
+    date: 'date',
+    array: ['number'],
+    object: { prop: 'number' },
+    array_of_objects: [ { prop: 'number' } ],
+    nested: { object: { prop: 'number' } },
+    optional: {
+      __value: 'number',
+      __optional: true,
     },
-    staggering: [{
-      volume: 'number',
-      price: 'number',
-    }],
-    updated: 'date',
+    any_optional: {
+      __value: 'any',
+      __optional: true,
+    }
   });
 
   describe('#new Schema()', function() {
@@ -25,97 +29,142 @@ describe('Schema', function() {
   });
 
   describe('#validate()', function() {
-    it('should successfully validate the object (any + optional)', function() {
-      const anySchema = new Schema({
-        prop: {
-          __value: 'any',
-          __optional: true,
-        },
+    describe('successful validation', function() {
+      it('any', function() {
+        const schema = new Schema({ any: 'any' });
+        const results = [];
+        results.push(schema.validate({ any: 'string' }));
+        results.push(schema.validate({ any: 42 }));
+        results.push(schema.validate({ any: [{ prop: 42 }] }));
+        expect(results).to.only.contain(true);
       });
-      const results = [];
-      results.push(anySchema.validate({}));
-      results.push(anySchema.validate({ prop: 'string' }));
-      results.push(anySchema.validate({ prop: 42 }));
-      results.push(anySchema.validate({ prop: [{ prop: 42 }] }));
-      expect(results).to.not.contain(false);
+
+      it('string', function() {
+        const schema = new Schema({ string: 'string' });
+        const result = schema.validate({ string: 'string' });
+        expect(result).to.be.ok();
+      });
+
+      it('number', function() {
+        const schema = new Schema({ number: 'number' });
+        const result = schema.validate({ number: 42 });
+        expect(result).to.be.ok();
+      });
+
+      it('boolean', function() {
+        const schema = new Schema({ boolean: 'boolean' });
+        const result = schema.validate({ boolean: false });
+        expect(result).to.be.ok();
+      });
+
+      it('date', function() {
+        const schema = new Schema({ date: 'date' });
+        const result = schema.validate({ date: new Date() });
+        expect(result).to.be.ok();
+      });
+
+      it('array', function() {
+        const schema = new Schema({ array: ['number'] });
+        const result = schema.validate({ array: [ 1, 2, 3 ] });
+        expect(result).to.be.ok();
+      });
+
+      it('object', function() {
+        const schema = new Schema({ object: { prop: 'number' } });
+        const result = schema.validate({ object: { prop: 42 } });
+        expect(result).to.be.ok();
+      });
+
+      it('array of objects', function() {
+        const schema = new Schema({
+          array_of_objects: [{
+            prop: 'number',
+          }],
+        });
+        const result = schema.validate({
+          array_of_objects: [{
+            prop: 42,
+          }, {
+            prop: 43,
+          }],
+        });
+        expect(result).to.be.ok();
+      });
+
+      it('nested object', function() {
+        const schema = new Schema({ nested: { object: { prop: 'number' } } });
+        const result = schema.validate({ nested: { object: { prop: 42 } } });
+        expect(result).to.be.ok();
+      });
+
+      it('optional', function() {
+        const schema = new Schema({
+          optional: {
+            __value: 'number',
+            __optional: true,
+          }
+        });
+        const results = [];
+        results.push(schema.validate({}));
+        results.push(schema.validate({ optional: 42 }));
+        expect(results).to.only.contain(true);
+      });
+
+      it('any & optional', function() {
+        const schema = new Schema({
+          any_optional: {
+            __value: 'any',
+            __optional: true,
+          },
+        });
+        const results = [];
+        results.push(schema.validate({}));
+        results.push(schema.validate({ any_optional: 'string' }));
+        results.push(schema.validate({ any_optional: 42 }));
+        results.push(schema.validate({ any_optional: [{ prop: 42 }] }));
+        expect(results).to.only.contain(true);
+      });
     });
 
-    const schema = createSchema();
-
-    it('should successfully validate the object', function() {
-      const result = schema.validate({
-        price: {
-          min: 5,
-          max: 10,
-        },
-        staggering: [{
-          volume: 5,
-          price: 7,
-        }],
-        updated: new Date(),
+    describe('erroneous validation', function() {
+      it('mismatched type', function() {
+        const schema1 = new Schema({ string: 'string' });
+        const schema2 = new Schema({ array: [] });
+        const schema3 = new Schema({ array_of_objects: [{ prop: 'number' }] });
+        const results = [];
+        results.push(schema1.validate({ string: 42 }));
+        results.push(schema2.validate({ array: { prop: 42 } }));
+        results.push(schema3.validate({ array_of_objects: [ 42 ] }));
+        expect(results).to.only.contain(false);
       });
-      expect(result).to.be.ok();
-    });
-
-    it('should fail to validate the object (mismatched type)', function() {
-      const result = schema.validate({
-        price: {
-          min: 5,
-          max: 'ten',
-        },
-        staggering: [{
-          volume: 5,
-          price: 'seven',
-        }],
-        updated: new Date(),
+  
+      it('missing property', function() {
+        const schema = new Schema({ prop: 'any' });
+        const result = schema.validate({});
+        expect(result).to.not.be.ok();
       });
-      expect(result).to.not.be.ok();
-    });
-
-    it('should fail to validate the object (missing property)', function() {
-      const result = schema.validate({
-        price: {
-          min: 5,
-        },
-        staggering: [{
-          volume: 5,
-        }],
-        updated: new Date(),
+  
+      it('surplus property', function() {
+        const schema = new Schema({ prop: 'any' });
+        const result = schema.validate({
+          prop: 42,
+          surplusProp: 43,
+        });
+        expect(result).to.not.be.ok();
       });
-      expect(result).to.not.be.ok();
-    });
-
-    it('should fail to validate the object (surplus property)', function() {
-      const result = schema.validate({
-        price: {
-          min: 5,
-          max: 10,
-          avg: 7.5,
-        },
-        staggering: [{
-          volume: 1,
-          price: 10,
-          surplusProperty: true,
-        }],
-        updated: new Date(),
+  
+      it('mismatched type on optional property', function() {
+        const schema = new Schema({
+          prop: {
+            __value: 'number',
+            __optional: true
+          }
+        });
+        const results = [];
+        results.push(!schema.validate({}));
+        results.push(schema.validate({ prop: 'string' }));
+        expect(results).to.only.contain(false);
       });
-      expect(result).to.not.be.ok();
-    });
-
-    it('should fail to validate the object (mismatched type on optional property)', function() {
-      const result = schema.validate({
-        price: {
-          min: 5,
-          max: 10,
-          avg: 'median',
-        },
-        staggering: [{
-          volume: 1,
-          price: 10,
-        }],
-        updated: new Date(),
-      });
-      expect(result).to.not.be.ok();
     });
   });
 });
